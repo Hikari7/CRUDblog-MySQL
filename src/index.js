e96e954074887fb86f3e911cec9b7d54edfc6c4a
Hikari7 <h.kobe712@gmail.com>
2023-01-23 22:56:31 -0800

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 require("dotenv").config();
 const express = require("express");
-
-const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
 
 //mysqlを読み込ませる
 const dbConnection = require("./util/mysql");
 
-// Route handler that sends a message at /
+const app = express();
 
+//.useはmiddleware(req, resの仲介)の設定
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+//クライアントにアクセスさせたい静的ファイルが格納されているフォルダを設定
+app.use(express.static(path.join(__dirname, "public")));
+
+//exporessの初期化設定(拡張子を省略して記述できる)
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+// Route handler that sends a message at /
 app.get("/", (req, res) => {
   res.send("Hello Express!");
+});
+
+app.get("/blogs", () => {
+//routerにつなげて、処理の内容たいはmodelに書いていく
+  
+  res.json();
 });
 
 const PORT = process.env.PORT || 8000;
@@ -21,5 +39,6 @@ app.listen(PORT, async () => {
   const [data] = await dbConnection.query("SELECT 1"); //{"1":1}  resulting the value of "SELECT 1"
   //retrun the first element: data, and the second element: metadata
   //   console.log(connect);
-  if (data) console.log("Successful connection to the MySQL database!");
+  //   if (data)
+  //   console.log("Successful connection to the MySQL database!");
 });
